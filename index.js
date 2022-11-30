45663d7c4abb1d47574becbc576b3f5b60cd55f7
ETU936 <80760102+ToavinaM@users.noreply.github.com>
2022-11-30 19:39:31 +0300

    ertyj

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ app.use("/user", UsersRouter);
 app.use("/ticket", TicketsRouter);
 app.use("/role", RolesRouter);
 app.use("/file", FilesRouter);
-
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 http.listen(PORT, () => {
   console.log(`Serveur à l écoute de ${PORT}`)
